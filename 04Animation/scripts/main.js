43a2ec141483763e869c06795207512c3850c628
Jules Brouwer <jules.brouwer@gmail.com>
2020-05-12 22:02:19 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,16 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin() {
-	myOtherBox.object3D.rotation.xyz -= rotationSpeed;
- 	console.log(myOtherBox.object3D.rotation.xyz);
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.x);
+ }
+ function spin() {
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.y);
+ }
+ function spin() {
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.z);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
